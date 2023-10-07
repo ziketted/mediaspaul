b0617b7712353eb5d5a60e418dfdd14e0ba1a794
Joelmkendi <53572084+Joelmkendi@users.noreply.github.com>
2023-10-07 21:28:13 +0100

    operation_finish

--- resources/views/operations/create.blade.php
+++ resources/views/operations/create.blade.php
@@ -1,49 +1,18 @@
 @include('header.header')
 <div class="wrapper">
+
+    <form action="{{route('operation.store')}}" method="POST">
+        @csrf
     <div class="container-fluid">
         <div class="row m-5 p-3 ">
             <!-- Basic example -->
-            <div class="col-lg-10 mt-5">
+            <div class="col-lg-6 mt-4">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="m-t-0 m-b-30">Opération de caisse.</h4>
+                        <h4 class="m-t-0 m-b-2">Opération de caisse  <code class="text-nowrap" style="width: 12rem; color:black">{{ $caisse }}</code>.</h4>
 
-                        <form action="{{route('operation.store')}}" method="POST">
-                            @csrf
                             <div class="row">
-                                <div class="col-lg-6" style="padding-right: 50px; padding-left:50px;">
-                                    <label>Moyen de paiement</label>
-
-                                    <div>
-                                        <code class="text-nowrap" style="width: 12rem; color:black">Selectionner le moyen de paiement</code>
-                                        <select class="form-control txt-20" name="moyen" aria-label="Default select example" required>
-                                            <option value="Cash" selected>Cash</option>
-                                            <option value="Cheque">Chèque</option>
-                                        </select>
-
-                                    </div>
-                                </div>
-                                <div class="col-lg-6" style="padding-right: 50px; padding-left:50px;">
-                                    <div class="form-group">
-                                        <div class="form-group">
-                                            <label>Caisse</label>
-                                            <div>
-                                                <code class="text-nowrap" style="width: 12rem; color:black">{{ $caisse }}</code>
-                                                <select class="form-control" name="devise" aria-label="Default select example" >
-                                                    <option value="{{ $caisse }}" selected >Caisse Principale {{ $caisse }}</option>
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-
-                            </div>
-                            <hr>
-                            <div class="row mt-2">
-                                <div class="col-lg-4" style="border-right: 1px #F0F0F0 solid; padding-right: 50px; padding-left: 50px;">
-
-
+                                <div class="col-lg-12" style="padding-right: 50px; padding-left:50px;">
 
                                     <div class="form-group">
                                         <label>Date opération</label>
@@ -51,12 +20,32 @@
                                             <div class="input-group">
                                                 <input type="date" name="date" class="form-control" placeholder="mm/dd/yyyy"
                                                     id="datepicker" required>
-
                                             </div><!-- input-group -->
                                         </div>
                                     </div>
+
                                     <div class="form-group">
-                                        <label>Type opération</label>
+                                        <label>Béneficiare</label>
+                                        <div>
+                                            <div class="input-group">
+                                                <input type="text" name="beneficiare" class="form-control" placeholder="Noms du Béneficiaire" required>
+                                            </div><!-- input-group -->
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="pe-5">Moyen de paiement</label>
+                                        <select class="form-control txt-20" name="moyen" aria-label="Default select example" required>
+                                            <option value="Cash" selected>Cash</option>
+                                            <option value="Cheque">Chèque</option>
+                                        </select>
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-12" style="padding-right: 50px; padding-left:50px;">
+                                   
+                                    <div class="form-group">
+                                        <label>Type de transaction</label>
                                         <div>
                                             <select class="form-control" name="type" aria-label="Default select example" required>
                                                 <option value="Entrée" selected>Entrée</option>
@@ -65,8 +54,7 @@
                                             </select>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-lg-4" style="padding-right: 50px; padding-left: 50px; ">
+
                                     <div class="form-group">
                                         <label>Secteur</label>
                                         <div>
@@ -77,6 +65,7 @@
                                             </select>
                                         </div>
                                     </div>
+
                                     <div class="form-group">
                                         <label>Financement</label>
                                         <div>
@@ -93,47 +82,62 @@
                                             </select>
                                         </div>
                                     </div>
-
                                 </div>
-                                <div class="col-lg-4" style="border-left: 1px #F0F0F0 solid; padding-right: 50px; padding-left: 50px; ">
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">Libellé de l'opération</label>
-                                        <input type="text" name="libelle" class="form-control" id="exampleInputEmail1"
-                                            placeholder="Libellé de l'opération" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">Montant</label>
-                                        <input type="number" name="montant" class="form-control" id="exampleInputEmail1"
-                                            placeholder="Ex.100$" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleInputPassword1">Description</label>
-                                        <input type="text" name="description" class="form-control" id="exampleInputPassword1"
-                                            placeholder="Description de l'opération">
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <button type="submit" class="btn btn-primary waves-effect waves-light mt-5">Enregistrer</button>
-                        </form>
+                            </div> 
+                      
                     </div><!-- card-body -->
                 </div> <!-- card -->
             </div> <!-- col-->
-            <div class="col-lg-2 mt-5">
+            <div class="col-lg-6 mt-4">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="m-t-0 ">Opérations en attente de validation.</h4>
-                        <center>
-                            <h1>5</h1>
-                        </center>
-                        <a href="/waiting" class="btn btn-primary w-100">Voir</a>
+                        <h4 class="m-t-0 ">Détails de l'opération.</h4>
+                        <div class="ps-5 pe-5" style="">
+                            <table  class="table table-striped table-bordered dt-responsive nowrap"
+                            style="border-collapse: collapse; width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th>Compte</th>
+                                    <th>Libelle</th>
+                                    <th>Montant</th>
+                                    <th class="text-center w-25">Option</th>
+                                </tr>
+                            </thead>
+    
+                            <tbody>
+                                <tr>
+                                    <td class="p-0 bg-white">
+                                        <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Compte" name="compte[]">
+                                        <datalist id="datalistOptions">
+                                          <option value="San Francisco">
+                                          <option value="New York">
+                                          <option value="Seattle">
+                                          <option value="Los Angeles">
+                                    </td>
+                                    <td class="p-0 bg-white">
+                                        <input type="text" name="libelle[]" id="" placeholder="Libelle" class="form-control w-100 h-100 border-0">
+                                    </td>
+                                    <td class="p-0 bg-white">
+                                        <input type="number" name="montant[]" id="" placeholder="En {{strtolower($caisse)}}" class=" form-control w-100 h-100 border-0">
+                                    </td>
+                                    <td class="w-25 text-center p-0 btn-info">
+                                        <button class="btn btn-info mx-auto w-100"
+                                            onclick="if (!confirm('Voulez-vous vraiment supprimer cet élément ?')) { return false }"><i class="mdi mdi-plus"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                        <button class="btn btn-success w-100 text-uppercase" type="submit">Valider l'opération</button>
                     </div><!-- card-body -->
                 </div> <!-- card -->
             </div>
         </div>
     </div><!-- End row -->
+</form>
 </div>
 <!-- end wrapper -->
-{{-- @include('header.footer')
- --}}
+<div class="d-none">
+    @include('header.footer')
+</div>
--- resources/views/operations/waiting.blade.php
+++ resources/views/operations/waiting.blade.php
@@ -5,50 +5,52 @@
             <!-- Basic example -->
             <div class="col-lg-12 mt-5">
                 <div class="card">
-                    <div class="card-body">
+                    <div class="card-body p-5">
                         <h4 class="m-t-0 m-b-30">Opération en attente de validation.</h4>
 
-                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap"
-                        style="border-collapse: collapse; width: 100%;">
-                        <thead>
-                            <tr>
-                                <th>Numéro</th>
-                                <th>Date opération</th>
-                                <th>Désignation</th>
-                                <th>Bénéficiaire</th>
-                                <th>Type</th>
-                                <th>Montant</th>
-                                <th>options</th>
-                            </tr>
-                        </thead>
-
-
-                        <tbody>
-                        {{--     @foreach ($operations as $item) --}}
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td>
-                                    <a class="btn btn-secondary text-white">Voir </a>
-                                    <form action="" method="POST"
-                                        style="display:inline;">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button class="btn btn-danger"
-                                            onclick="if (!confirm('Voulez-vous vraiment supprimer cet élément ?')) { return false }">Supprimer
-                                        </button>
-                                    </form>
-                                </td>
-
-                            </tr>
-                           {{--  @endforeach --}}
-
-                        </tbody>
-                    </table>
+                   
+                
+                                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap"
+                                            style="border-collapse: collapse; width: 100%;">
+                                            <thead>
+                                                <tr>
+                                                    <th>Numéro</th>
+                                                    <th>Date opération</th>
+                                                    <th>Désignation</th>
+                                                    <th>Bénéficiaire</th>
+                                                    <th>Type</th>
+                                                    <th>Montant</th>
+                                                    <th>options</th>
+                                                </tr>
+                                            </thead>
+                
+                
+                                            <tbody>
+                                               
+                                                <tr>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td>
+                                                        <a class="btn btn-secondary text-white" href="#">Voir </a>
+                                                        
+                                                            <button class="btn btn-danger"
+                                                                onclick="if (!confirm('Voulez-vous vraiment supprimer cet élément ?')) { return false }">Supprimer
+                                                            </button>
+                                                        </a>
+                                                    </td>
+                
+                                                </tr>
+                                               
+                
+                                            </tbody>
+                                        </table>
+                
+                
+                     
                     </div><!-- card-body -->
                 </div> <!-- card -->
             </div> <!-- col-->          
@@ -56,5 +58,4 @@
     </div><!-- End row -->
 </div>
 <!-- end wrapper -->
-{{-- @include('header.footer')
- --}}
+@include('header.footer')
