9bf220d62e1ecbf090820f6bb8894d148c387453
Joelmkendi <53572084+Joelmkendi@users.noreply.github.com>
2023-10-07 22:48:22 +0100

    op.fin

--- resources/views/operations/create.blade.php
+++ resources/views/operations/create.blade.php
@@ -107,7 +107,7 @@
                                     <!-- Ligne initiale -->
                                     <tr>
                                         <td class="p-0 bg-white">
-                                            <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Compte" name="compte_id[]">
+                                            <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Compte" name="compte_id[]" required>
                                             <datalist id="datalistOptions">
                                                 <option value="San Francisco">
                                                 <option value="New York">
@@ -116,10 +116,10 @@
                                             </datalist>
                                         </td>
                                         <td class="p-0 bg-white">
-                                            <input type="text" name="libelle[]" placeholder="Libelle" class="form-control w-100 h-100 border-0">
+                                            <input type="text" name="libelle[]" placeholder="Libelle" class="form-control w-100 h-100 border-0" required>
                                         </td>
                                         <td class="p-0 bg-white">
-                                            <input type="number" name="montant[]" placeholder="En {{strtolower($caisse)}}" class="form-control w-100 h-100 border-0">
+                                            <input type="number" name="montant[]" placeholder="En {{strtolower($caisse)}}" class="form-control w-100 h-100 border-0" required>
                                         </td>
                                         <td class="w-25 text-center p-0">
                                             <a class="btn btn-info mx-auto w-100 addRowBtn"><i class="mdi mdi-plus"></i></a>
@@ -128,6 +128,7 @@
                                 </tbody>
                             </table>
                         </div>
+                        <h3 class="fw-bold"><span id="totalSum"></span> {{$caisse}}</h3>
                         <button class="btn btn-success w-100 text-uppercase" type="submit">Valider l'opération</button>
                     </div><!-- card-body -->
                 </div> <!-- card -->
@@ -137,55 +138,79 @@
 </form>
 </div>
 <script>
-    $(document).ready(function () {
-        // Gestionnaire d'événement pour le bouton d'ajout de ligne
-        $(document).on('click', '.addRowBtn', function () {
-            addRow(this);
+ $(document).ready(function () {
+    function updateTotalSum() {
+        var total = 0;
+        $('input[name="montant[]"]').each(function () {
+            var value = parseFloat($(this).val());
+            if (!isNaN(value)) {
+                total += value;
+            }
         });
 
-        // Fonction pour ajouter une nouvelle ligne
-        function addRow(button) {
-            // Créer une nouvelle ligne
-            var table = document.getElementById("myTable").getElementsByTagName('tbody')[0];
-            var row = table.insertRow(table.rows.length);
+        // Mettre à jour le paragraphe avec la somme totale
+        $('#totalSum').text('Total : ' + total);
+    }
 
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            var cell3 = row.insertCell(2);
-            var cell4 = row.insertCell(3);
-
-            cell1.className = "p-0 bg-white";
-            cell2.className = "p-0 bg-white";
-            cell3.className = "p-0 bg-white";
-            cell4.className = "w-25 text-center p-0";
-
-            cell1.innerHTML = '<input class="form-control" list="datalistOptions" placeholder="Compte" name="compte_id[]"><datalist id="datalistOptions"><option value="San Francisco"><option value="New York"><option value="Seattle"><option value="Los Angeles"></datalist>';
-            cell2.innerHTML = '<input type="text" name="libelle[]" placeholder="Libelle" class="form-control w-100 h-100 border-0">';
-            cell3.innerHTML = '<input type="number" name="montant[]" placeholder="En {{strtolower($caisse)}}" class="form-control w-100 h-100 border-0">';
-            cell4.innerHTML = '<a class="btn btn-danger mx-auto w-100 deleteRowBtn"><i class="mdi mdi-minus"></i></a>';
-
-            // Désactiver le bouton "+" sur toutes les lignes précédentes
-            $('.addRowBtn').removeClass('addRowBtn').addClass('deleteRowBtn');
-            $('.deleteRowBtn i').removeClass('mdi mdi-plus').addClass('mdi mdi-minus');
-            $('.deleteRowBtn').removeClass('btn-info').addClass('btn-danger');
-            // Réactiver le bouton "+" sur la dernière ligne
-            $(row).find('.deleteRowBtn').removeClass('deleteRowBtn').addClass('addRowBtn');
-            $(row).find('.addRowBtn i').removeClass('mdi mdi-minus').addClass('mdi mdi-plus');
-            $(row).find('.addRowBtn').removeClass('btn-danger').addClass('btn-info');
-        }
-
-        // Gestionnaire d'événement pour le bouton de suppression de ligne
-        $(document).on('click', '.deleteRowBtn', function () {
-            removeRow(this);
-        });
-
-        // Fonction pour supprimer une ligne
-        function removeRow(button) {
-            var row = button.parentNode.parentNode;
-            row.parentNode.removeChild(row);
-        }
+    // Gestionnaire d'événement pour le bouton d'ajout de ligne
+    $(document).on('click', '.addRowBtn', function () {
+        addRow(this);
     });
+
+    // Gestionnaire d'événement pour le bouton de suppression de ligne
+    $(document).on('click', '.deleteRowBtn', function () {
+        removeRow(this);
+        updateTotalSum(); // Mettre à jour le montant total après la suppression
+    });
+
+    // Gestionnaire d'événement pour la modification de la valeur de l'input
+    $(document).on('input', 'input[name="montant[]"]', function () {
+        updateTotalSum();
+    });
+
+    // Fonction pour ajouter une nouvelle ligne
+    function addRow(button) {
+        // Créer une nouvelle ligne
+        var table = document.getElementById("myTable").getElementsByTagName('tbody')[0];
+        var row = table.insertRow(table.rows.length);
+
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+
+        cell1.className = "p-0 bg-white";
+        cell2.className = "p-0 bg-white";
+        cell3.className = "p-0 bg-white";
+        cell4.className = "w-25 text-center p-0";
+
+        cell1.innerHTML = '<input class="form-control" list="datalistOptions" placeholder="Compte" name="compte_id[]" required><datalist id="datalistOptions"><option value="San Francisco"><option value="New York"><option value="Seattle"><option value="Los Angeles"></datalist>';
+        cell2.innerHTML = '<input type="text" name="libelle[]" placeholder="Libelle" class="form-control w-100 h-100 border-0" required>';
+        cell3.innerHTML = '<input type="number" name="montant[]" placeholder="En {{strtolower($caisse)}}" class="form-control w-100 h-100 border-0" required>';
+        cell4.innerHTML = '<a class="btn btn-danger mx-auto w-100 deleteRowBtn"><i class="mdi mdi-minus"></i></a>';
+
+        // Désactiver le bouton "+" sur toutes les lignes précédentes
+        $('.addRowBtn').removeClass('addRowBtn').addClass('deleteRowBtn');
+        $('.deleteRowBtn i').removeClass('mdi mdi-plus').addClass('mdi mdi-minus');
+        $('.deleteRowBtn').removeClass('btn-info').addClass('btn-danger');
+        // Réactiver le bouton "+" sur la dernière ligne
+        $(row).find('.deleteRowBtn').removeClass('deleteRowBtn').addClass('addRowBtn');
+        $(row).find('.addRowBtn i').removeClass('mdi mdi-minus').addClass('mdi mdi-plus');
+        $(row).find('.addRowBtn').removeClass('btn-danger').addClass('btn-info');
+    }
+
+    // Fonction pour supprimer une ligne
+    function removeRow(button) {
+        var row = button.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+    }
+
+    // Appel initial pour mettre à jour la somme totale
+    updateTotalSum();
+});
+
 </script>
+
 <!-- end wrapper -->
 <div class="d-none">
     @include('header.footer')
